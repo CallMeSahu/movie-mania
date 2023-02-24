7edbbea7b7082663fb0a7ce1c33163509e291430
Siddhartha <siddharthasahu.work@gmail.com>
2023-02-25 01:58:12 +0530

    feat: search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import Card from './components/Card';
+import React, {useState} from 'react';
+import Card from './components/Card/Card';
+import Search from './components/Search/Search';
 
 function App() {
+  const[keyword, setKeyword] = useState('');
+
   return (
     <div className="App">
       <h1 className='App-header'>Movie Mania</h1>
-      <Card />
+        <Search setKeyword={setKeyword} />
     </div>
   );
 }
